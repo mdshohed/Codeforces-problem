4db1ba318f35b6cbdf85123845b3120d822bdfef
Shohedul <49224753+mdshohed@users.noreply.github.com>
2020-04-15 14:49:05 +0600

    Update Round#130 ( Div 2: A ).cpp

--- Round#130 ( Div 2: A ).cpp
+++ Round#130 ( Div 2: A ).cpp
@@ -9,3 +9,22 @@ int main() {
     cout << regex_replace( s, p, " ") << '\n';
     return 0;
 }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    string x = "shohed";
+
+    while ( s.find ( x ) != -1 ) {
+        s.replace( s.find(x) , x.size(), " " );
+    }
+    stringstream ss(s);
+    while ( ss >> s) {
+        cout << s << " ";
+    }
+    return 0;
+}
