6de61220404f831b4e629b1fb44398aad5c0ebff
Shohedul <49224753+mdshohed@users.noreply.github.com>
2020-04-16 16:17:24 +0600

    Create  100: 3n + 1 problem.cpp

---
+++ UVA Judge/100: 3n + 1 problem.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int cycle ( ll n ) {
+    int x = 1;
+    while ( n !=1 ) {
+        if ( n%2==0 ){
+            n = n/2;
+        }
+        else {
+            n = 3*n + 1;
+        }
+        x++;
+    }
+    return x;
+}
+
+int main() {
+    ll a, b;
+    while ( scanf( "%lld%lld", &a, &b) != EOF ) {
+        ll x1 = a, x2 = b;
+        int mx = 0, t = 0;
+        if( a>b) {
+            for ( int i= a; i >=b; i--) {
+                t = cycle(i);
+                if ( t >= mx) mx = t;
+            }
+        }
+        else {
+            for ( int i= b; i>=a; i--) {
+                t = cycle(i);
+                if ( t >= mx) mx = t;
+            }
+        }
+        cout << x1 << " " << x2 << " "<< mx << '\n';
+    }
+    return 0;
+}
