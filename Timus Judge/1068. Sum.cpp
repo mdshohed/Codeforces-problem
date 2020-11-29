ead005392b1dc84da3aef8d39e22ec096eabbd3d
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2020-11-29 11:13:47 +0600

    Create 1068. Sum.cpp

---
+++ Timus Judge/1068. Sum.cpp
@@ -0,0 +1,34 @@
+/**
+    problem: 1068. Sum
+    algorithm: ...
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+     ios::sync_with_stdio(false),cin.tie(nullptr);
+
+    int t = 1;
+   // cin >> t;
+    while (t--) {
+
+       ll n, sum = 0;
+       cin >> n;
+       if ( n>0) {
+            for (int i = 1; i<=n; i++) {
+                sum += i;
+            }
+            cout << sum << endl;
+       }
+       else {
+
+            for (int i = n; i<=1; i++) {
+                sum += i;
+            }
+            cout << sum << endl;
+       }
+    }
+    return 0;
+}
